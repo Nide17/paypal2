48b758887aee5ad2d0860f4633fc7f960f0f0cc1
Nide17 <nidedrogba@gmail.com>
2020-08-02 10:03:09 +0200

    Updates

---
+++ precache-manifest.380cecbc9196c53192c95f0297cb8a57.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593bc518e591ca82d66ef8782749ef6d",
+    "url": "/paypal2/index.html"
+  },
+  {
+    "revision": "5a016d5766ba6b8956cf",
+    "url": "/paypal2/static/css/2.fd7f0f65.chunk.css"
+  },
+  {
+    "revision": "2dbb1ad63d1d5cd42d04",
+    "url": "/paypal2/static/css/main.9e4303fe.chunk.css"
+  },
+  {
+    "revision": "5a016d5766ba6b8956cf",
+    "url": "/paypal2/static/js/2.bd454f59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/paypal2/static/js/2.bd454f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dbb1ad63d1d5cd42d04",
+    "url": "/paypal2/static/js/main.17cf876d.chunk.js"
+  },
+  {
+    "revision": "cbf54eeae37ed6447c2f",
+    "url": "/paypal2/static/js/runtime-main.bb55ad7d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/paypal2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/paypal2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/paypal2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/paypal2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/paypal2/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fc23e79afa48e5d7232d15e74805a9e5",
+    "url": "/paypal2/static/media/video_542x308px.fc23e79a.jpg"
+  }
+]);
